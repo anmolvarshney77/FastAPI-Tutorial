3743babdb722e0fa5c93ab9bbc08f6b07e5c8f10
Anmol Varshney <varshney.anmol.29@gmail.com>
2026-03-07 23:12:04 +0530

    New Commit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "My name is FastAPI! Didn't Complete this code till now!"}
+    return {"Hello": "My name is FastAPI!"}
